43d8965e1e69124da8a45e9f8f29dcf3c620554f
RealManPy <96335081+RealManPy@users.noreply.github.com>
2024-04-16 15:32:49 +0300

    Add files via upload
    added message to notify user when they are wrong
    added question 2.7
    added option for images to be displayed

--- script.js
+++ script.js
@@ -12,6 +12,15 @@ const questions = [
 		, ".פקח עירוני אינו מוסמך להורות לנהגים על הסדרי תנועה"],
         answer: ".כן, אם ההוראות קשורות לסדרי התנועה"
     },
+	    { 
+        question: '?האם מותר לך לעקוף כלי רכב איטי הנוסע לפניך כשלשמאלך מסומן קו הפרדה רצוף',
+		img: "images/2.7.PNG",
+        choices: [".מותר לך לעקוף אותו, בתנאי שלא תחצה את קו ההפרדה הרצוף"
+		, ".מותר לך לחצות את קו ההפרדה הרצוף ולעקוף את כלי הרכב האיטי אם הוא נוסע לאט במשך זמן רב"
+		,".מותר לך לעקוף את כלי הרכב האיטי שלפניך רק אם הוא כלי רכב לא מנועי"
+		, ".מותר לך לעקוף אותו ואף לחצות את קו ההפרדה הרצוף, אלא אם כן יש תמרור מיוחד האוסר זאת"],
+        answer: ".מותר לך לעקוף אותו, בתנאי שלא תחצה את קו ההפרדה הרצוף"
+    },
     // Add more questions here
 ];
 
@@ -27,6 +36,7 @@ progress.setAttribute("max", questions.length + "");
 
 
 function displayQuestion() {
+	
     const currentQuestion = questions[currentQuestionIndex];
     questionElement.textContent = currentQuestion.question;
     choicesElement.innerHTML = ""; // Clear previous choices
@@ -40,6 +50,12 @@ function displayQuestion() {
         });
         choicesElement.appendChild(choiceButton);
     });
+	if (currentQuestion.img){
+		const imgElement = document.createElement("img");
+		imgElement.src = currentQuestion.img;
+		imgElement.style.height = "300px";
+		choicesElement.appendChild(imgElement);
+	}
 }
 
 function checkAnswer(selectedChoice) {
@@ -48,13 +64,28 @@ function checkAnswer(selectedChoice) {
         currentQuestionIndex++;
         if (currentQuestionIndex < questions.length) {
             displayQuestion();
-            
+        }
+        // Remove the error message if it exists
+        const errorMessage = resultElement.querySelector("h1");
+        if (errorMessage) {
+            errorMessage.remove();
+        }
+    } else {
+        // Check if error message already exists
+        let errorMessage = resultElement.querySelector("h1");
+        if (!errorMessage) {
+            errorMessage = document.createElement("h1");
+            errorMessage.style.color = "red";
+            errorMessage.textContent = "טעות. נסו שוב";
+            resultElement.appendChild(errorMessage); // Append the message to the resultElement
         }
     }
-	moveCar();
-	progress.setAttribute("value", currentQuestionIndex + "");
-	
+    moveCar();
+    progress.setAttribute("value", currentQuestionIndex + "");
 }
+
+
+
 
 
 
